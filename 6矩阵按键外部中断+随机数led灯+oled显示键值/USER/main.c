af6b6c3455dd200b9141f6725a2e4337d72b0a56
pottor <249272221@qq.com>
2022-04-06 16:26:08 +0800

    Add files via upload

---
+++ 6矩阵按键外部中断+随机数led灯+oled显示键值/USER/main.c
@@ -0,0 +1,56 @@
+#include "led.h"
+#include "delay.h"
+#include "sys.h"
+#include "usart.h"
+#include "exti.h"//4x4æÿ’Û∞¥º¸Õ‚≤ø÷–∂œ
+#include "oled.h"
+#include "random_generate.h"//…˙≥…ÀÊª˙ ˝
+
+
+ int main(void)
+ {		
+	extern int KeyValue;
+
+	 
+	delay_init();	    	 //—” ±∫Ø ˝≥ı ºªØ	  
+	NVIC_PriorityGroupConfig(NVIC_PriorityGroup_2); //…Ë÷√NVIC÷–∂œ∑÷◊È2:2Œª«¿’º”≈œ»º∂£¨2ŒªœÏ”¶”≈œ»º∂
+	uart_init(115200);	 //¥Æø⁄≤®Ãÿ¬ 115200
+ 	LED_Init();		  		 //≥ı ºªØ”ÎLED¡¨Ω”µƒ”≤º˛Ω”ø⁄
+	OLED_Init();			   //≥ı ºªØOLED  
+	OLED_Clear();  
+	EXTIX_Init();		 	   //Õ‚≤ø÷–∂œ≥ı ºª
+	Adc_Init();          //adc≥ı ºªØ
+
+
+	KeyValue=0;
+	 
+	 
+	OLED_ShowCHinese(0,0,0);//÷–
+	OLED_ShowCHinese(18,0,1);//æ∞
+	OLED_ShowCHinese(36,0,2);//‘∞
+	OLED_ShowCHinese(54,0,3);//µÁ
+	OLED_ShowCHinese(72,0,4);//◊”
+	OLED_ShowCHinese(90,0,5);//ø∆
+	OLED_ShowCHinese(108,0,6);//ºº
+	
+
+
+	while(1)
+	{
+		led_output();
+		delay_ms(50);
+//	OLED_Clear();
+		OLED_ShowCHinese(0,0,0);//÷–
+		OLED_ShowCHinese(18,0,1);//æ∞
+		OLED_ShowCHinese(36,0,2);//‘∞
+		OLED_ShowCHinese(54,0,3);//µÁ 
+		OLED_ShowCHinese(72,0,4);//◊”
+		OLED_ShowCHinese(90,0,5);//ø∆
+		OLED_ShowCHinese(108,0,6);//ºº
+		
+		OLED_ShowNum(0,2,KeyValue,8,8);
+//	OLED_ShowNum(0,4,randomValue,8,8);
+	
+	}
+ }
+
